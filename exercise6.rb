597f2459a8e021700d841ca9e383de5745f9ace4
nadiaivannova <nadiaivannova@gmail.com>
2017-06-27 15:25:55 -0400

    farentheit formula working

---
+++ exercise6.rb
@@ -0,0 +1,18 @@
+#(F - 32) x 5/9
+#to_i
+
+#Start with prompting the user for a temperature in Fahrenheit. Then call your method and pass in the user input as a parameter.
+
+#Output the result in a full sentence using string interpolation.
+
+
+
+#What are the inputs required to do the work inside the method?
+#  - user inputed fahrentiheit number
+
+#What do I want to get out of the method?
+ #- fahrentiheit converted to celcious
+
+ puts "what is the temperature in fahrentiheit?"
+ fahrentiheit = gets.to_i
+ puts "The weather is #{(fahrentiheit - 32) * 5/9} degrees celcious"
