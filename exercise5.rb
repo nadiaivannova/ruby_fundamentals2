5a4dff4936642c6c249811851b0013d7ed46f5ce
nadiaivannova <nadiaivannova@gmail.com>
2017-06-27 15:06:41 -0400

    reverse name method added

---
+++ exercise5.rb
@@ -0,0 +1,18 @@
+
+#What are the inputs required to do the work inside the method?
+#  - name
+
+#What do I want to get out of the method?
+ #- greting with reversed name
+
+
+def greet_backwards(name)
+
+puts "Hello, #{name.reverse}! Welcome home."
+end
+
+greet_backwards("Amanda")
+greet_backwards("Bob")
+greet_backwards("Shirly")
+greet_backwards("Sue")
+greet_backwards("Andy")
